9a127e02432a90cce278d49a4a4a9182e54e67d0
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-08-11 09:58:09 +0300

    Working with dynamic parameters

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,46 @@
-import path from 'path'
-import fs from 'fs/promises'
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
 
 const HomePage = (props) => {
+    const { products } = props;
 
-  const { products } = props;
-
-  return (
-    <div >
-      <ul>
-        {products.map(prod => <li key={prod.id}>{prod.title}</li>)}
-      </ul>
-    </div>
-  )
-}
+    return (
+        <div>
+            <ul>
+                {products.map((prod) => (
+                    <li key={prod.id}>
+                        <Link href={`${prod.id}`}>{prod.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export async function getStaticProps(context) {
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json")
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData)
+    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
 
-  if (!data) {
-    return {
-      redirect: {
-        destination: '/no-data'
-      }
+    if (!data) {
+        return {
+            redirect: {
+                destination: "/no-data",
+            },
+        };
     }
-  }
 
-  if (data.products.length === 0) {
-    return { notFound: true }
-  }
+    if (data.products.length === 0) {
+        return { notFound: true };
+    }
 
-  return {
-    props: {
-      products: data.products
-    },
-    revalidate: 10 // how often the page regenetares in production
-  };
+    return {
+        props: {
+            products: data.products,
+        },
+        revalidate: 10, // how often the page regenetares in production
+    };
 }
 
 export default HomePage;
