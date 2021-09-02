e9d024d464cd56083b36c5b54991f54ca82a5eb1
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-09-02 21:39:56 +0300

    working with fallback pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -5,6 +5,10 @@ import fs from "fs/promises";
 const ProductDetailPage = (props) => {
     const { loadedProduct } = props;
 
+    // if (!loadedProduct) {
+    //     return <p>Loading...</p>;
+    // }
+
     return (
         <>
             <h1>{loadedProduct.title}</h1>
@@ -30,10 +34,10 @@ export async function getStaticPaths() {
     return {
         paths: [
             { params: { pid: "p1" } },
-            { params: { pid: "p2" } },
-            { params: { pid: "p3" } },
+            // { params: { pid: "p2" } },
+            // { params: { pid: "p3" } },
         ],
-        fallback: false,
+        fallback: "blocking",
     };
 }
 
