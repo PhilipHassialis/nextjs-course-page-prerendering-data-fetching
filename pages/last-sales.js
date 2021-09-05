5825546dab5616f1674ca8e100520aee908a3269
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-09-05 22:57:34 +0300

    implementing client-side data fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const LastSalesPage = () => {
-    useEffect(() => {}, []);
+    const [sales, setSales] = useState();
+    const [isLoading, setIsLoading] = useState(false);
 
-    return <ul></ul>;
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+            "https://page-prerendering-data-fetch-default-rtdb.firebaseio.com/sales.json"
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                const transformedSales = [];
+                for (const key in data) {
+                    transformedSales.push({
+                        id: key,
+                        username: data[key].username,
+                        volume: data[key].volume,
+                    });
+                }
+                setSales(transformedSales);
+                setIsLoading(false);
+            });
+    }, []);
+
+    if (isLoading) return <p>Loading...</p>;
+    if (!sales) return <p>No data yet...</p>;
+
+    return (
+        <ul>
+            {sales &&
+                sales.map((sale) => (
+                    <li key={sale.id}>
+                        {sale.username} - {sale.volume}
+                    </li>
+                ))}
+        </ul>
+    );
 };
 
 export default LastSalesPage;
