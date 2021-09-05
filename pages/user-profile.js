a55eddf075c5381a7581140a275646b33d9e68e0
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-09-05 16:55:23 +0300

    getServerSideProps and its context

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -7,6 +7,11 @@ const UserProfilePage = (props) => {
 export default UserProfilePage;
 
 export async function getServerSideProps(context) {
+    const { params, req, res } = context;
+
+    // console.log(req); default node objects for request / response
+    // console.log(res);
+
     return {
         props: {
             userName: "Philip",
