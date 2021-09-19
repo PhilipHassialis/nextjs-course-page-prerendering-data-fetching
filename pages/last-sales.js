5ab6ada54a700ad9589fc856945e552d8917fa51
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-09-20 01:15:46 +0300

    combining pre rendering with client data fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const LastSalesPage = () => {
-    const [sales, setSales] = useState();
+const LastSalesPage = (props) => {
+    const [sales, setSales] = useState(props.sales);
     // const [isLoading, setIsLoading] = useState(false);
 
     const fetcher = (url) => fetch(url).then((r) => r.json());
@@ -50,7 +50,7 @@ const LastSalesPage = () => {
     console.log(data);
 
     if (error) return <p>Failed to load</p>;
-    if (!data) return <p>Still Loading...</p>;
+    if (!data && !sales) return <p>Loading...</p>;
 
     // if (isLoading) return <p>Loading...</p>;
     if (!sales) return <p>No data yet...</p>;
@@ -66,5 +66,21 @@ const LastSalesPage = () => {
         </ul>
     );
 };
+
+export async function getStaticProps(ctx) {
+    const response = await fetch(
+        "https://page-prerendering-data-fetch-default-rtdb.firebaseio.com/sales.json"
+    );
+    const data = await response.json();
+    const transformedSales = [];
+    for (const key in data) {
+        transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+        });
+    }
+    return { props: { sales: transformedSales }, revalidate: 600 };
+}
 
 export default LastSalesPage;
